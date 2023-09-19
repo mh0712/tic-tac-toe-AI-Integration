c279b5490f02ecff793fd16a01e9b7af20980580
marc <marchajjar05@gmail.com>
2023-09-19 22:47:54 +0300

    Basic tic tac toe edition

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+import React from "react";
+import TicTacToe from "./components/TicTacToe";
+import './scss/TicTacToe.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <TicTacToe />
+    </div>
+  )
+}
+
+export default App;
